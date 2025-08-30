39c880b803928b3e1048afb3d62b7ab88ae4ca1c
Amal Ragab <158066611+AmalRagab@users.noreply.github.com>
2025-08-31 01:21:29 +0300

    Update Patient.h

--- Patient.h
+++ Patient.h
@@ -1,1 +1,22 @@
+#ifndef PATIENT_H
+#define PATIENT_H
 
+#include "Person.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Patient : public Person {
+private:
+    string disease;
+
+public:
+    Patient(string name, char gender, int age, int id ,string disease);
+
+    void setDisease(string d);
+    string getDisease() const;
+
+    void display() const override;
+};
+#endif // PATIENT_H
