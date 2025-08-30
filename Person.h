28ba6667404fd8f9bdb59ce667db92ffb3b714c9
Amal Ragab <158066611+AmalRagab@users.noreply.github.com>
2025-08-31 00:33:55 +0300

    Create Person.h

---
+++ Person.h
@@ -0,0 +1,35 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+
+class Person
+{
+
+protected:
+	string name;
+	char gender;
+	int age, id;
+	
+	
+public:
+	//setter
+	void set_name(string name);
+	void set_gender(char gender);
+	void set_age(int age);
+	void set_id(int id);
+
+	//getter
+	string get_name() const;
+	char get_gender() const;
+	int get_age() const;
+	int get_id() const;
+	
+	virtual void display() const;
+	
+	//cons and des
+	Person();	
+	Person(string name, char gender, int age, int id);
+    virtual ~Person();
+};
+
+#endif // PERSON_H
