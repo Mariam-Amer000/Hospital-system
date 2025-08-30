92923ef803bd83f4525b04b60fa2c35b6b387384
Amal Ragab <158066611+AmalRagab@users.noreply.github.com>
2025-08-31 01:36:28 +0300

    Update Patient.h

--- Patient.h
+++ Patient.h
@@ -10,12 +10,16 @@ using namespace std;
 class Patient : public Person {
 private:
     string disease;
+    vector<string> medicalHistory;
 
 public:
     Patient(string name, char gender, int age, int id ,string disease);
 
     void setDisease(string d);
     string getDisease() const;
+
+    void addReport(string repo);
+    void showHistory() const;
 
     void display() const override;
 };
