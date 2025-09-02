bfb2012d80926fb114a4b10edf982660006dda37
Amal Ragab <158066611+AmalRagab@users.noreply.github.com>
2025-09-02 15:03:07 +0300

    Update Patient.h

--- Patient.h
+++ Patient.h
@@ -1,28 +1,30 @@
-#ifndef PATIENT_H
-#define PATIENT_H
 
+#pragma once
 #include "Person.h"
 #include <iostream>
 #include <string>
 #include <vector>
+#include "DataManager.h"
+class Doctor; 
 
 using namespace std;
 
 
 class Patient : public Person {
 private:
+    DataManager dataManager;
     string disease;
     vector<string> medicalHistory;
 
 public:
     Patient(string name, char gender, int age, int id, string disease);
+    Patient() : Person("", ' ', 0, 0), disease("") {} // Default constructor
 
     void setDisease(string d);
     string getDisease() const;
 
-    void addReport(string repo);
-    void showHistory() const;
+    void addReport(Patient& patient, Doctor* doctor);
+    void showHistory(int Patient_id) const;
 
     void display() const override;
 };
-#endif // PATIENT_H
